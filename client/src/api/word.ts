693639151cc45c1ce307e795d500dd8a3c90fe6a
shanaka11 <shanakaabeysinghe@gmail.com>
2023-02-19 12:26:02 +0530

    Changed word.ts

--- client/src/api/word.ts
+++ client/src/api/word.ts
@@ -2,12 +2,12 @@ import axios from "axios"
 import { IWord } from "../types/IWord"
 
 const getWords = async () => {
-    return axios.get<IWord[]>(`${process.env.PORT}word`)
+    return axios.get<IWord[]>(`${process.env.VITE_BACKEND_URL}word`)
 }
 
 const addWords = async (data:IWord) => {
     return axios.post<IWord>(
-        `${process.env.PORT}}word`,
+        `${process.env.VITE_BACKEND_URL}}word`,
         data
     )
 }
